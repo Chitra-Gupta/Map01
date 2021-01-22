014353025790a0f290e1cad5df4b1bd9603c55e7
Chitra-Gupta <chitrachanchal111@gmail.com>
2021-01-22 13:47:21 +0430

    Map

---
+++ app/src/main/java/chitra/helloworld/mapapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package chitra.helloworld.mapapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map=googleMap;
+        LatLng Bihar=new LatLng(25.8917265,83.5577553);
+        map.addMarker(new MarkerOptions().position(Bihar).title("Bihar"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Bihar));
+
+    }
+}
